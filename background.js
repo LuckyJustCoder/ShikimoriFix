5d79c771fab9f8074c9834d9e223c4ae60dc9521
LuckyJustCoder <lakujust@gmail.com>
2019-05-18 00:07:36 +0300

    Вот теперь действительно все нормально :3

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
+
+chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
+	chrome.tabs.executeScript(details.tabId, {file:"contentscript.js", runAt: 'document_end' })
+});
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {method: "addButton"}, function(response) {
-            console.log("Injection ready!");
-        });
-    });
+		chrome.tabs.sendMessage(tabs[0].id, {method: "addButton"}, function(response) {
+		});
+	});
 });
