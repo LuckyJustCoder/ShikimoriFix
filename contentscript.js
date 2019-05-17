2534595c5a2542dfbe4a21f8071d04ec13cbf0db
LuckyJustCoder <lakujust@gmail.com>
2019-05-18 00:13:25 +0300

    Вот теперь действительно все нормально :3

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,14 @@
-// contentscript.js
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.method == "addButton") {
-			var actualCode = `if(!$('.watch_link').length )$(".watch-online-placeholer").append("<a class='b-link_button dark watch_link watch-online'>Смотреть онлайн</a>");$(".watch_link").attr("href", window.gon.watch_online.watch_url); `;
-			var script = document.createElement('script');
-			script.textContent = actualCode;
-			(document.head||document.documentElement).appendChild(script);
-			script.remove();
+			var url = window.location.href;
+			if(url.indexOf('https://shikimori.one/animes/') != -1)
+				var actualCode = `if(!$('.watch_link').length )$(".watch-online-placeholer").append("<a class='b-link_button dark watch_link watch-online'>Смотреть онлайн</a>");$(".watch_link").attr("href", window.gon.watch_online.watch_url); `;
+				var script = document.createElement('script');
+				script.textContent = actualCode;
+				(document.head||document.documentElement).appendChild(script);
+				script.remove();
         } 
     }
 );
