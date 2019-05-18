cc1c77e9ce27157b48aba029349bfa80d4539fab
LakuJust <44136772+LuckyJustCoder@users.noreply.github.com>
2019-05-18 13:26:39 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+
+	$.ajax({
+		url:'https://raw.githubusercontent.com/LuckyJustCoder/ShikimoriFix/master/config.json',
+		success: function (data){
+			let manifestData = chrome.runtime.getManifest();
+
+			if(JSON.parse(data)['actual_version'] === manifestData.version){
+				$('.version').text('Версия: ' + manifestData.version);
+			}else{
+				$('.version').text('Версия: ' + manifestData.version + ' устарела, актуальная версия: ' + JSON.parse(data)['actual_version']+" [ПКМ]");
+				$('.version').css('color', 'red');
+			}
+			$('.update').attr('href', "https://github.com/LuckyJustCoder/ShikimoriFix");
+		}
+	});
 	$('body').on('click', 'a', function(){
 		chrome.tabs.create({url: $(this).attr('href')});
 		return false;
