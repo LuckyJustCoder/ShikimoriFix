88f71d587ce7f54835a70cd61033c9ea5180fd68
LuckyJustCoder <lakujust@gmail.com>
2019-05-18 13:55:01 +0300

    криво убраны ошибки в консоль

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 
 chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
-	chrome.tabs.executeScript(details.tabId, {file:"contentscript.js", runAt: 'document_end' })
+	chrome.tabs.executeScript(details.tabId, {file: "contentscript.js", runAt: 'document_end'})
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {method: "addButton"}, function(response) {
 		});
