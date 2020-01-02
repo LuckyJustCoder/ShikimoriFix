2e39125ba59aca849e5743954cc46802afd530d6
LakuJust <44136772+LuckyJustCoder@users.noreply.github.com>
2020-01-02 21:19:23 +0300

    Update injected.js

--- public/js/injected.js
+++ public/js/injected.js
@@ -1,5 +1,8 @@
+let link;
+
 document.addEventListener("yourCustomEvent", function (e) {
-	let props = JSON.parse(e.detail);
+	let cofg = JSON.parse(e.detail);
+	link = cofg.link;
 
 	if (!$(".watch-online-placeholer").length) {
 		$(".c-info-right").append("<div class='watch-online-placeholer'></div>");
@@ -11,33 +14,27 @@ document.addEventListener("yourCustomEvent", function (e) {
 
 	if (!$(".watch_link").length) {
 
-		$(".watch-online-placeholer").append(
-				"<div class='block'><a class='b-link_button dark watch_link watch-online'>Смотреть онлайн</a></div>"
-		);
-
-		let episode = (parseInt($(".current-episodes").text()) || 0) + 1;
-		if (parseInt($(".total-episodes").text()) < parseInt(episode) - 1) {
-			episode = 1;
-		}
-
 		let id = window.location.href
 				.split("/")[4]
 				.split("-")[0]
 				.replace(/[^-0-9]/gim, "");
-		let total = $(".total-episodes").text();
-		let title = $(".inline")
-				.parent()
-				.text();
-		$(".watch_link")
-			.attr("target", "_blank")
-			.attr("href", `https://app.nekomori.ch/player/${id}`);		
-// 		if (props['open_with'] === true) {
 
-// 		} else {
-// 			$(".watch_link").attr(
-// 					"href",
-// 					`${props['link']}#/${id}/${episode}/?total=${total}&title=${title}`
-// 			);
-// 		}
+		fetch('https://api.nekomori.ch/players/link?artId=' + (-id) + '&from=https://nekomori.ch/')
+				.then(response => response.json())
+				.then(data => {
+					if (!$(".watch-neko").length) {
+						$(".watch-online-placeholer").append(
+								"<div class='block watch-neko'><a href='" + data.bodyText + "' class='b-link_button dark watch_link watch-online'>Смотреть онлайн</a></div>"
+						);
+					}
+				}).catch((err) => {
+			let name = $('.head > h1').text().split("/")[0];
+			if (!$(".watch-neko").length) {
+				$(".watch-online-placeholer").append(
+						"<div class='block watch-neko'><a href='https://nekomori.ch/search?kind=anime&season=0&ep=all&search=" + name + "' class='b-link_button dark watch_link watch-online'>Найти на nekomori.ch</a></div>"
+				);
+			}
+		});
+
 	}
 });
