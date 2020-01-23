ddd848fe208e4b259fbceea1eac95948f496984a
LakuJust <44136772+LuckyJustCoder@users.noreply.github.com>
2020-01-23 07:34:14 +0300

    Update injected.js

--- public/js/injected.js
+++ public/js/injected.js
@@ -13,28 +13,11 @@ document.addEventListener("yourCustomEvent", function (e) {
 	}
 
 	if (!$(".watch_link").length) {
-
-		let id = window.location.href
-				.split("/")[4]
-				.split("-")[0]
-				.replace(/[^-0-9]/gim, "");
-
-		fetch('https://api.nekomori.ch/players/link?artId=' + (-id) + '&from=https://nekomori.ch/')
-				.then(response => response.json())
-				.then(data => {
-					if (!$(".watch-neko").length) {
-						$(".watch-online-placeholer").append(
-								"<div class='block watch-neko'><a href='" + data.bodyText + "' class='b-link_button dark watch_link watch-online'>Смотреть онлайн</a></div>"
-						);
-					}
-				}).catch((err) => {
-			let name = $('.head > h1').text().split("/")[0];
-			if (!$(".watch-neko").length) {
-				$(".watch-online-placeholer").append(
-						"<div class='block watch-neko'><a href='https://nekomori.ch/search?kind=anime&season=0&ep=all&search=" + name + "' class='b-link_button dark watch_link watch-online'>Найти на nekomori.ch</a></div>"
-				);
-			}
-		});
-
+		if (!$(".watch-neko").length) {
+			let name = $('.head > h1').text().split("/")[1];
+			$(".watch-online-placeholer").append(
+					"<div class='block watch-neko'><a href='https://nekomori.ch/search?kind=anime&season=0&ep=all&search=" + name + "' class='b-link_button dark watch_link watch-online'>Найти на nekomori.ch</a></div>"
+			);
+		}
 	}
 });
