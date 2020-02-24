92d660c30099f7cc14508d0e560797d5d0ce80f8
LakuJust <44136772+LuckyJustCoder@users.noreply.github.com>
2020-02-24 17:05:38 +0300

    Update injected.js

--- public/js/injected.js
+++ public/js/injected.js
@@ -12,7 +12,7 @@ document.addEventListener("yourCustomEvent", function (e) {
 		if (!$(".watch-neko").length) {
 			let name = $('.head > h1').text().split("/")[1];
 			$(".watch-online-placeholer").append(
-					"<div class='block watch-neko'><a href='https://nekomori.ch/search?kind=anime&season=0&ep=all&search=" + name + "' class='b-link_button dark watch_link watch-online'>Найти на nekomori.ch</a></div>"
+					"<div class='block watch-neko'><a href='https://nekomori.ch/search?type=anime&kind=0&season=0&ep=all&search=" + name + "' class='b-link_button dark watch_link watch-online'>Найти на nekomori.ch</a></div>"
 			);
 		}
 	}
